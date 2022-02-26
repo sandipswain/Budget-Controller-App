8b971255892997926d43b1e1f97260b5f71c073a
Sandip Swain <swainsandip9@gmail.com>
2022-02-26 14:21:10 +0530

    Done with Adding budgets

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
+import { useState } from "react";
 import { Button, Container, Stack } from "react-bootstrap";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
+import { useBudgets } from "./contexts/BudgetsContext";
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets } = useBudgets();
   return (
-    <Container>
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budget</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
-        }}
-      >
-        <BudgetCard
-          name="Entertainment"
-          amount={1200}
-          max={1000}
-          gray
-        ></BudgetCard>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budget</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          {budgets.map((budget) => (
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={budget.amount}
+              max={budget.max}
+              gray
+            />
+          ))}
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
   );
 }
 
---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import { useBudgets } from "./../contexts/BudgetsContext";
+
+function AddBudgetModal({ show, handleClose }) {
+  const nameRef = useRef();
+  const maxRef = useRef();
+  const { addBudget } = useBudgets();
+  function handleSubmit(e) {
+    e.preventDefault();
+    addBudget({
+      name: nameRef.current.value,
+      max: parseFloat(maxRef.current.value),
+    });
+    handleClose();
+  }
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Budget</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="max">
+            <Form.Label>Maximum Spending</Form.Label>
+            <Form.Control
+              type="number"
+              ref={maxRef}
+              min={0}
+              step={0.01}
+              required
+            />
+          </Form.Group>
+          <div className="d-flex justify-content-end">
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </div>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  );
+}
+
+export default AddBudgetModal;
